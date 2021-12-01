5c696922f1face06054cc17dafbd2ec1d3d2ae33
BrianN <ngoyi.brian1050@gmail.com>
2021-12-01 10:00:58 +0100

    ajout du lien vers pictureclick html vers le fichier index

---
+++ pictureclick.js
@@ -0,0 +1,7 @@
+let images = document.querySelectorAll("#left img") 
+let main = document.querySelector("#main")
+for (let i = 0; i<images.length; i++) {
+images[i].addEventListener("click", function(event) {
+    main.src = event.target.src
+})
+}
